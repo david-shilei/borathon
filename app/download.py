9c7b2e6d2536b5ed9907b1dd415e92f7a2cc7b8f
Tao Xie <txie@vmware.com>
2014-06-19 23:17:47 +0800

    module download.py

--- app/download.py
+++ app/download.py
@@ -1,9 +1,13 @@
 from urllib2 import urlopen
 import re
+import os.path
+import tarfile
+import os
+import zipfile
 from bs4 import BeautifulSoup  
 
 #test config
-url="http://engweb.eng.vmware.com/bugs/files/0/1/2/6/3/8/3/2/"
+url="http://engweb.eng.vmware.com/bugs/files/0/1/2/4/9/0/9/7/"
 localdir="/tmp/"
 
 def getTgzFiles(url):
@@ -17,19 +21,49 @@ def getTgzFiles(url):
 def downloadFile(url, filename):
 	req = urlopen(url + filename)
 	local_filename = localdir + filename
+
+	if(os.path.exists(local_filename)):
+		print "%s already exists, does not download again" % local_filename
+
 	CHUNK = 16*1024
 	with open(local_filename, 'wb') as f:
-		for chunk in req.read(CHUNK):
-			if chunk:
-				f.write(chunk)
-				f.flush()
-		localfile.close()
+		while True:
+			chunk = req.read(CHUNK)
+			if not chunk: break
+			f.write(chunk)
+		f.flush()
+		f.close()
 		return local_filename
 
 def downloadFiles(url, filelist):
 	for filename in filelist:
 		downloadFile(url, filename)
 
+def extractFile(path, to_directory='.'):
+    if path.endswith('.zip'):
+        opener, mode = zipfile.ZipFile, 'r'
+    elif path.endswith('.tar.gz') or path.endswith('.tgz'):
+        opener, mode = tarfile.open, 'r:gz'
+    elif path.endswith('.tar.bz2') or path.endswith('.tbz'):
+        opener, mode = tarfile.open, 'r:bz2'
+    else: 
+        raise ValueError, "Could not extract `%s` as no appropriate extractor is found" % path
+    
+    cwd = os.getcwd()
+    os.chdir(to_directory)
+    
+    try:
+        file = opener(path, mode)
+        try: file.extractall()
+        finally: file.close()
+    finally:
+        os.chdir(cwd)
+
+def extractFiles(files, to_dir):
+	for file in files:
+		extractFile(localdir + file, to_dir)
+
 if __name__ == '__main__':
 	files = getTgzFiles(url)
 	downloadFiles(url, files)
+	extractFiles(files, localdir)
