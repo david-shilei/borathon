f8536fe9884e03d78f395c42a13dcb0419ae6e77
David Shi <dshi@vmware.com>
2014-06-24 16:08:54 +0800

    Update poc to use badge

--- assets/js/borathon.js
+++ assets/js/borathon.js
@@ -1,4 +1,37 @@
-$(document).ready(function(){
+// var myObject = eval('(' + myJSONtext + ')');
+// var myJSONText = JSON.stringify(myObject, replacer);
+// function replacer(key, value) {
+//     if (typeof value === 'number' && !isFinite(value)) {
+//         return String(value);
+//     }
+//     return value;
+// }
+
+var json_result = {
+"entities": [
+ 	{
+		"name": "host-9:91:76e1b7f8-0-13e9",
+		"logs": [ 
+			{
+				"time": 1231231231123,
+				"line": 234,
+				"file": "hostd.log",
+ 			    "content":"2014-05-09T23:31:24.304Z [321C2B70 info 'Vmsvc.vm:/vmfs/volumes/vsan:52a7980961ea0ddf-e8c7fef416d27ea0/484c6d53-e861-52ac-6e8c-2c44fd7c2d24/io-10.139.130.110-vsanDatastore-rhel6-64-vmwpv-lc-0028.vmx' opID=host-9:91:76e1b7f8-0-13e9 user=vpxuser] Failed to load virtual machine: vim.fault.FileNotFound."
+			}]
+	},
+	{
+		"name": "host-9:94:22480d43-0-1ae8",
+		"logs": [
+			{
+				"time": 1231231231231,
+				"line": 235,
+				"file": "hostd.log",
+				"content": "2014-05-09T23:31:24.304Z [321C2B70 info 'Vmsvc.vm:/vmfs/volumes/vsan:52a7980961ea0ddf-e8c7fef416d27ea0/484c6d53-e861-52ac-6e8c-2c44fd7c2d24/io-10.139.130.110-vsanDatastore-rhel6-64-vmwpv-lc-0028.vmx' opID=host-9:91:76e1b7f8-0-13e9 user=vpxuser] Failed to load virtual machine: vim.fault.FileNotFound."
+			}]
+	}
+]}
+
+$(document).ready(function(){	
 	var data = [{"content":"RecordOp ASSIGN: guest, 53. Sent notification immediately.","loglevel":"verbose","start":1399706542422},{"content":"RecordOp ASSIGN: summary.guest, 17. Sent notification immediately.","loglevel":"verbose","start":1399706546913},{"content":"Done Dispatching Vigor callback","loglevel":"verbose","start":1399706556078}]
 	
 	// $.get( "http://ec2-54-254-246-134.ap-southeast-1.compute.amazonaws.com/borathon/hostd.log.json", function( data, status) {
@@ -6,21 +39,41 @@ $(document).ready(function(){
 	// });
 	//
 	data = data.slice(0, 5)
-  	$.each(data,function(idx,item){
-		time = new Date(item.start);
-		var newItem = $('<li style="display: none;"><a class="entity-link" href="#" >' + item.content  + '</a></li>');
-		var link = newItem.children(".entity-link")
-		link.data("time", time);
-		link.data("content", item.content);
-		link.data("loglevel", item.loglevel);
+  	$.each(json_result["entities"], function(idx, entity){
+		var link = $("<a />", {
+		    href: "#",
+		    "class": "entity-link", // you need to quote "class" since it's a reserved keyword
+		    text: entity.name,
+			title: entity.name,
+			style: "float:left"
+		});
+		link.data("name", entity.name);
+		link.data("logs", entity.logs);
+		
+		var badge = $("<span />", {
+			"class": "badge",
+			text: entity.logs.length,
+			style: "float:right"
+		});
+		
+		var newItem = $("<li />", {
+		    style: "display: none; clear: both;"
+		});
+		newItem.append(link);
+		newItem.append(badge);
+		
 		newItem.appendTo($("#entityList")).show('slow');
   	});
 	  
-	  
   	$(".entity-link").click(function(){
-  		var contentContainer = $('#entityContent');
-  		contentContainer.empty();
-          var content = $('<div class="span4" style="display: none;"><h2>' + $(this).data("content") +'</h2><p>' + $(this).data("time")  + '</p></div><!--/span-->');
-    	  content.appendTo(contentContainer).show('slow');
+		var log = $(this).data("logs")[0]
+  		displayLog(log);
   	});
 });
+
+function displayLog(log) {
+	var contentContainer = $('#entityContent');
+	contentContainer.empty();
+    var logContent = $('<div class="span4" style="display: none;"><h2>' + new Date(log.time) +'</h2><p>' +  log.content + '</p></div><!--/span-->');
+	logContent.appendTo(contentContainer).show('slow');
+}
