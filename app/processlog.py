68424f5c36299f34946f596eb03c5c4df4d41f17
Tao Xie <txie@vmware.com>
2014-06-26 23:55:03 +0800

    fix merge bug

--- app/processlog.py
+++ app/processlog.py
@@ -150,7 +150,7 @@ def extractLogLines(file, entity, entity_log_mapping):
                     entity_log_mapping[entity].append(record)
             lineNumber += 1
 
-def extractEntities(file, conf, entities):
+def extractEntities(file, conf, entities, pr):
     if not os.path.exists(file):
         return
     with open(file) as f:
